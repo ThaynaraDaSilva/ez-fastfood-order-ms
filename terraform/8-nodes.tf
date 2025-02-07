e9d8778c6e3ed2613963de19693a0deb29c70ade
Thaynara Silva <thaynara.silva@yaman.com.br>
2025-02-07 16:59:30 -0300

    correção de security group no terraform eks e nodes

--- terraform/8-nodes.tf
+++ terraform/8-nodes.tf
@@ -1,3 +1,30 @@
+resource "aws_security_group" "eks_nodes_sg" {
+  name        = "eks-nodes-sg"
+  description = "Security Group for EKS Worker Nodes"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_cluster_sg.id]  # FIXED: Worker nodes talk to EKS API
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_iam_role" "nodes" {
   name = "${local.eks_name}-eks-nodes-${local.env}"
 
