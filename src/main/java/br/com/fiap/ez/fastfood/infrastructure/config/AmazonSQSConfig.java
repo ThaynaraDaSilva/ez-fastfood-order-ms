329e8d3047d712bde4fb38396856d8ae93d0357b
Thaynara Silva <thaynara.silva@yaman.com.br>
2025-02-16 17:20:33 -0300

    remoção de dependencia de teste e ajuste do AmazonSQSConfig

--- src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/AmazonSQSConfig.java
+++ src/main/java/br/com/fiap/ez/fastfood/infrastructure/config/AmazonSQSConfig.java
@@ -1,6 +1,6 @@
 package br.com.fiap.ez.fastfood.infrastructure.config;
 
-import java.net.URI;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+
 
 
 @Configuration
@@ -22,11 +21,12 @@ public class AmazonSQSConfig {
 	        this.amazonSQSProperties = amazonSQSProperties;
 	       
 	    }
+	    
 	    @Bean
-	    public SqsAsyncClient sqsAsyncClient() {
-	        return SqsAsyncClient.builder()
-	                .region(Region.of(amazonSQSProperties.getRegion()))
-	                .credentialsProvider(DefaultCredentialsProvider.create()) // Usa credenciais padrão da AWS
+	    public SqsClient sqsClient() {
+	        return SqsClient.builder()
+	                .region(Region.of(amazonSQSProperties.getRegion())) // Mantendo a região configurável
+	                .credentialsProvider(DefaultCredentialsProvider.create()) // Usa credenciais do ambiente AWS automaticamente
 	                .build();
 	    }
 
